efe8488a2f8b4fbba1e1d3905458a1999af5071f
atarazavi <32979933+atarazavi@users.noreply.github.com>
2021-05-02 20:51:13 +0430

    Context provider + a hook for fetching tasks

--- src/Context/useTasksContextValue.tsx
+++ src/Context/useTasksContextValue.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { TasksContextData, TaskType, LogsType } from "../Context/TasksContext";
+import { TasksContextData, TaskType, LogsType } from "Context/TasksContext";
 import axios from "axios";
 
 export default function useTasksContextValue(): TasksContextData {
