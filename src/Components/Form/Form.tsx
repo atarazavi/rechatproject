e4ea5508758f3fb438abd7b75ef21c7cd98ff189
atarazavi <32979933+atarazavi@users.noreply.github.com>
2021-05-02 21:02:15 +0430

    Adding new task is working
    and updating tasks list is working too

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+import CloseIcon from "@material-ui/icons/Close";
+import Collapse from "@material-ui/core/Collapse";
+import { TasksContext } from "Context/TasksContext";
+import useTasksLoading from "../TasksList/hooks/useTasksLoading";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,10 +28,29 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Form() {
   const classes = useStyles();
+  const [mode, setMode] = useState("add");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const { tasks, addTask, toBEditedTaskID } = useContext(
+    TasksContext
+  );
+  const reset = () => {
+    setMode("add");
+    setTitle("");
+    setDescription("");
+  };
+  useTasksLoading();
+  if (toBEditedTaskID && mode === "add" && tasks.length > 0) {
+    setMode("edit");
+    const toBEditedTaskObject = tasks.filter(
+      (e) => +e.id === +toBEditedTaskID
+    )[0];
+    setTitle(toBEditedTaskObject.name);
+    setDescription(toBEditedTaskObject.description);
+  }
   const AddOrSaveTask = () => {
-    return null;
+      title && description && addTask(title, description);
+    reset();
   };
 
   return (
