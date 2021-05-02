83ea8e93181aa713f847687fb2d9b9f7ffbf3dc5
atarazavi <32979933+atarazavi@users.noreply.github.com>
2021-05-02 16:01:14 +0430

    Basic folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Form from './Components/Form/Form';
+import TasksList from './Components/TasksList/TasksList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Form />
+      <br />
+      <TasksList />
     </div>
   );
 }
