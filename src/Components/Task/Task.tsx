15492b4232e807b726990d72c1c1cd7bb544ea5f
atarazavi <32979933+atarazavi@users.noreply.github.com>
2021-05-02 21:13:38 +0430

    Connected to Form/list for editing/logshowing

--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { useContext, Dispatch, SetStateAction } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
@@ -6,15 +6,16 @@ import Typography from "@material-ui/core/Typography";
 import EditIcon from "@material-ui/icons/Edit";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import Fab from "@material-ui/core/Fab";
+import { TasksContext } from "Context/TasksContext";
 import Button from "@material-ui/core/Button";
 // Types
 import { TaskType } from "Context/TasksContext";
 
 interface taskProps {
-    task: TaskType;
-    setLogMode?: Dispatch<SetStateAction<boolean>>;
-    setToBloggedID?: Dispatch<SetStateAction<number | null>>;
-  }
+  task: TaskType;
+  setLogMode?: Dispatch<SetStateAction<boolean>>;
+  setToBloggedID?: Dispatch<SetStateAction<number | null>>;
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -40,67 +41,64 @@ const useStyles = makeStyles((theme: Theme) =>
       position: "absolute",
       left: theme.spacing(0),
       bottom: theme.spacing(0),
-      color: "#000!important",
-    },
+      color: "#000!important"
+    }
   })
 );
 
-const Task: React.FC<taskProps> = ({ task }) => {
+const Task: React.FC<taskProps> = ({ task, setLogMode, setToBloggedID }) => {
   const classes = useStyles();
+  const { editThisID } = useContext(TasksContext);
   return (
-    <Paper style={{ padding: 10 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm container>
-          <Grid
-            item
-            container
-            direction="column"
-            spacing={2}
-            className={classes.taskContainer}
-          >
-            <Grid item xs>
-              <Typography noWrap gutterBottom variant="h5" component="h2">
-                {task.name}
-              </Typography>
-              <Typography
-                noWrap
-                variant="body2"
-                color="textSecondary"
-                component="p"
-              >
-                {task.name}
-              </Typography>
+      <Paper style={{ padding: 10 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm container>
+            <Grid
+              item
+              container
+              direction="column"
+              spacing={2}
+              className={classes.taskContainer}
+            >
+              <Grid item xs>
+                <Typography noWrap gutterBottom variant="h5" component="h2">
+                  {task.name}
+                </Typography>
+                <Typography
+                  noWrap
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                >
+                  {task.description}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button disabled disableElevation className={classes.statusElement} variant="contained">{task.status}</Button>
+              </Grid>
+              <div className={classes.root}>
+                <Fab
+                  onClick={() => editThisID(task.id)}
+                  color="secondary"
+                  size="small"
+                >
+                  <EditIcon />
+                </Fab>
+                <Fab
+                  size="small"
+                  className={classes.logFab}
+                  onClick={() => {
+                    setLogMode!(true);
+                    setToBloggedID!(task.id);
+                  }}
+                >
+                  <AssignmentIcon />
+                </Fab>
+              </div>
             </Grid>
-            <Grid item>
-              <Button
-                disabled
-                disableElevation
-                className={classes.statusElement}
-                variant="contained"
-              >
-                {task.name}
-              </Button>
-            </Grid>
-            <div className={classes.root}>
-              <Fab
-                onClick={() => console.log("0")}
-                color="secondary"
-                size="small"
-              >
-                <EditIcon />
-              </Fab>
-              <Fab
-                size="small"
-                className={classes.logFab}
-                onClick={() => console.log("1")}
-              >
-                <AssignmentIcon />
-              </Fab>
-            </div>
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
   );
 };
 
